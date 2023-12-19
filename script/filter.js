b6407aa99d700eb9c4cd9dfdbcf558c6c9faaa8c
davidtereshchenko <davakpacaba13@gmail.com>
2023-12-19 16:46:05 +0200

    fixed burger

--- script/filter.js
+++ script/filter.js
@@ -1,5 +1,5 @@
 document.querySelector('.filter__icon').addEventListener('click', function () {
-    this.classList.toggle('active');
+    this.classList.toggle('active-filter');
     document.querySelector('.filter__checkbox').classList.toggle('list');
-    document.querySelector('.filter__container').classList.toggle('open');
+    document.querySelector('.filter__container').classList.toggle('open-filter');
 });
